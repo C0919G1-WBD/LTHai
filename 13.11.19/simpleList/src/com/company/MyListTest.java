4689d64b364b5a8a26b3143b4732558b8b27c6e6
lethanhhai1998 <49983494+lethanhhai1998@users.noreply.github.com>
2019-11-13 11:47:35 +0700

    abc

---
+++ 13.11.19/simpleList/src/com/company/MyListTest.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class MyListTest {
+    public static void main(String[] args) {
+        MyList<Integer> listInteger = new MyList<Integer>();
+        listInteger.add(1);
+        listInteger.add(2);
+        listInteger.add(3);
+        listInteger.add(4);
+        listInteger.add(5);
+        listInteger.add(6);
+        listInteger.add(7);
+        listInteger.add(8);
+        listInteger.add(9);
+        listInteger.add(10);
+        listInteger.add(11);
+        listInteger.add(12);
+
+        System.out.println("element 4: " + listInteger.get(4));
+        System.out.println("element 1: " + listInteger.get(1));
+        System.out.println("element 2: " + listInteger.get(2));
+        System.out.println("element 10: " + listInteger.get(10));
+        System.out.println("element 11: " + listInteger.get(11));
+
+
+        System.out.println("element -1: " + listInteger.get(-1));
+    }
+}
