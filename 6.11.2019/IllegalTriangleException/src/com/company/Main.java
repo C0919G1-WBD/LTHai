446c3deb7751d11fc538f77c1ab80f682c4126c8
lethanhhai1998 <49983494+lethanhhai1998@users.noreply.github.com>
2019-11-18 08:43:05 +0700

    IllegalTriangleException

--- 6.11.2019/IllegalTriangleException/src/com/company/Main.java
+++ 6.11.2019/IllegalTriangleException/src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
 	// write your code here
         Scanner sc = new Scanner(System.in);
         System.out.print("Nhập cạnh a: ");
@@ -13,10 +13,10 @@ public class Main {
         int b = sc.nextInt();
         System.out.print("Nhập cạnh c: ");
         int c = sc.nextInt();
-        try {
-
-        } catch (Exception e) {
-            System.out.println();
+        if(a < 0 || b < 0 || c < 0) {
+            throw new Exception("Số đo 3 cạnh ko được nhỏ hơn 0");
+        } else if(a + b <= c || a + c <= b || b + c <= a) {
+            throw new Exception("Tổng hai cạnh không lớn hơn cạnh còn lại");
         }
     }
 }
